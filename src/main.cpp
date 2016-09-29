826421c79b6414f5b1e01fa12aa63e17ef8bb342
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2016-09-29 12:36:34 +0200

    New files added

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include "helper.h"
 #include "../my_inc/myadd.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int main() {
+int main(int argc, char** argv) {
   MyAdd add_obj;
-  std::cout << "Addition Result:" << add_obj.add(1,1) << std::endl;
+  long op[2] = {0, 0};
+  FILE *fp;
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  int count = 0;
+
+  fp = fopen(argv[1], "r");
+  if (!fp)
+    return 1;
+
+  while ((read = getline(&line, &len, fp)) != -1 && count < 2) {
+    count++;
+    op[count - 1] = strtol(line, NULL, 0);
+  }
+
+  fclose(fp);
+  if (line)
+    free(line);
+
+  std::cout << "Addition Result:" << add_obj.add(op[0],op[1]) << std::endl;
 }
