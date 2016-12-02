cabd2a09bd12968a5a24dd78587f759b722b5d19
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2016-12-02 15:52:04 +0100

    Added new test case for runtime unit testing

---
+++ src/main_no_abort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "helper.h"
+#include "../my_inc/myadd.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void bug() {
+  char *p = NULL;
+  *p = 4;
+}
+
+int main(int argc, char** argv) {
+  MyAdd add_obj;
+  long op[2] = {0, 0};
+  FILE *fp;
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  int count = 0, sum = 0;
+
+  fp = fopen(argv[1], "r");
+  if (!fp)
+    return 1;
+
+  while ((read = getline(&line, &len, fp)) != -1 && count < 2) {
+    count++;
+    op[count - 1] = strtol(line, NULL, 0);
+  }
+
+  fclose(fp);
+  if (line)
+    free(line);
+
+  sum = add_obj.add(op[0],op[1]);
+
+  if (sum == -1)
+    bug();
+
+  std::cout << "Addition Result:" << sum << std::endl;
+}
